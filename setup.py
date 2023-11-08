e2a2e84fcf84113569c4c2bad8acda1922822776
kcwoolverton <kcwoolverton@gmail.com>
2023-11-07 21:16:30 -0800

    added ranged attacks and combat maneuvers

--- setup.py
+++ setup.py
@@ -1,11 +1,4 @@
 import setuptools
-import os
-
-from coconut.api import cmd
-
-
-if __name__ == "__main__":
-    cmd([os.path.join(os.path.dirname(__file__), "pathfinder_character_sheet"), "--incremental"])
 
 setuptools.setup(
     name="pathfinder_character_sheet",
